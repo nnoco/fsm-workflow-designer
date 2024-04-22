483313e19f4b6ea24e0ad44c318529e2a52711a5
nnoco <junyoung.plum@gmail.com>
2024-04-23 00:37:29 +0900

    Hello, world 작성

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { Workflow, WorkflowDesigner } from '../types';
+
+export function FsmWorkflowDesigner(canvasId: string, options?: WorkflowDesigner.Options) {
+    const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+
+    if(!canvas) {
+        throw `Cannot found canvas by given id '${canvasId}'`;
+    }
+
+    if(!(canvas instanceof HTMLCanvasElement)) {
+        throw `'${canvasId}' is not type of HTMLCanvasElement'`;
+    }
+
+    if(options?.showGrid) {
+        const context = canvas.getContext('2d')!;
+
+        const gridSize = options?.gridSize ?? 20;
+        const { width, height } = canvas;
+
+        context.fillStyle = 'black';
+        for(let x = 0; x < width; x += gridSize) {
+            for(let y = 0; y < height; y += gridSize) {
+                context.fillRect(x, y, 1, 1);
+            }
+        }
+    }
+    
+    canvas.onclick = function(e) {
+        console.log('Hello, fsm-workflow-designer.');
+    }
+}
---
+++ types/index.d.ts
@@ -0,0 +1,14 @@
+namespace WorkflowDesigner {
+    export interface Options {
+        showGrid?: boolean;
+        gridSize?: number;
+    }
+}
+
+export interface Workflow<T> {
+    roots: State<T>[];
+}
+
+export interface State<T> {
+    payload?: T
+}
